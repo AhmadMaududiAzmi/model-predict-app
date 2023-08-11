1f9b822cca40cae25bb87ddbc64abde624f5729a
Maududi <maududi.azmi2702@gmail.com>
2023-08-11 19:57:27 +0700

    add grafik for ploting train, test and predict

--- grafik.py
+++ grafik.py
@@ -47,67 +47,12 @@ app = create_app()
 wsgi_app = app.wsgi_app
 db = Database(devconf)
 
-# ==============================================[ Routes - Start ]
-# Melakukan train model dengan menggunakan data train
-@app.route(f"{route_prefix}/traindata", methods=["GET", "POST"])
-def trainData():
+# =================================================[ Routes - End ]
+# Plot grafik hasil prediksi
+@app.route(f"{route_prefix}/plot", methods=['GET'])
+def plot():
     try:
-        query = "SELECT * FROM pertanian.harga_komoditas WHERE nm_komoditas = 'BAWANG PUTIH' AND nm_pasar = 'Pasar Dinoyo'"
-        records = db.run_query(query=query)
-        db.close_connection()
-
-        # Get dan parsing data menjadi time series
-        data = pd.DataFrame(records, columns=['id', 'tanggal', 'nm_pasar', 'nm_komoditas', 'id_komuditas', 'harga_current'])
-        data['tanggal'] = pd.to_datetime(data['tanggal'])
-        data['tanggal_epoch'] = data['tanggal'].apply(lambda x: x.timestamp())
-        data = data.sort_values('tanggal')
-
-        # Menghitung rata-rata tiap bulan untuk mengisi values harga_current = 0
-        data['tanggal'] = pd.to_datetime(data['tanggal'])
-        data['bulan'] = data['tanggal'].dt.month
-        monthly_avg = data.groupby('bulan')['harga_current'].mean()
-        data.loc[data['harga_current'] == 0, 'harga_current'] = data['bulan'].map(monthly_avg) 
-        
-        # # Normalisasi menggunakan MinMaxScaler()
-        scaler = MinMaxScaler()
-        data['harga_scaled'] = scaler.fit_transform(data[['harga_current']])
-
-        # Split data ke data train dan data test
-        X = data['tanggal_epoch'].values.reshape(-1, 1)
-        y = data['harga_scaled'].values
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-
-        # Model LSTM
-        model = Sequential()
-        model.add(LSTM(50, return_sequences=True, input_shape=(1, 1)))
-        model.add(LSTM(50))
-        model.add(Dense(25))
-        model.add(Dense(1))
-        model.compile(optimizer='adam', loss='mse')
-
-        # Train data
-        model.fit(X_train, y_train, epochs=100, batch_size=32)
-
-        # Simpan model LSTM ke dalam file sehingga saat penggunaan model saat prediksi tidak perlu train lagi
-        joblib.dump(model, 'trained_model.joblib')
-
-        # Response
-        # response = {
-        #     'message': 'Model successfully trained'
-        # }
-
-        # return jsonify(response)
-        return "Model trained successfuly"
-    except pymysql.MySQLError as err:
-        abort(HTTPStatus.INTERNAL_SERVER_ERROR, description=str(err))
-    except Exception as e:
-        abort(HTTPStatus.BAD_REQUEST, description=str(e))
-
-# Fungsi prediksi menggunakan LSTM
-@app.route(f"{route_prefix}/predict", methods=['GET'])
-def predict():
-    try:
-        query = "SELECT tanggal, harga_current FROM pertanian.daftar_harga WHERE tanggal >= '2016-01-01' AND tanggal <= '2020-12-31' AND nm_komoditas = 'Bawang Merah' AND nm_pasar = 'Pasar Dinoyo' GROUP BY tanggal"
+        query = "SELECT tanggal, harga_current FROM pertanian.daftar_harga WHERE tanggal >= '2016-01-01' AND tanggal <= '2020-12-31' AND nm_komoditas = 'Bawang Merah' AND nm_pasar = 'Pasar Senenan' GROUP BY tanggal"
         records = db.run_query(query=query)
         db.close_connection()
 
@@ -188,7 +133,7 @@ def predict():
         # Mengembalikan values data ke bentuk asal sebelum dinormalisasi
         predictions = scaler.inverse_transform(predictions) 
 
-        # Mengembalikan values data test ke bentuk asal sebelum normalisasi
+        # Mengembalikan nilai asli data test sebelum dinormalisasi
         yTest_original = scaler.inverse_transform(yTest.reshape(-1, 1))
 
         # Evaluasi model menggunakan RMSE
@@ -197,48 +142,29 @@ def predict():
         akurasi = "Root Mean Squared Error (RMSE) pada data test: {:.2f}".format(rmse)
 
         # Pembuatan dataframe setelah dilakukan pelatihan model
-        data_predictions = pd.DataFrame(predictions, columns=['predictions'], index=data.index).reset_index()
-        data_test_df = pd.DataFrame(yTest_original, columns=['scaled_harga_current'])
+        predicted_data = pd.DataFrame({'Predicted': predictions.flatten(), 'Actual': yTest_original.flatten()}, index=data.index[-len(predictions):])
+        
+        # Plot hasil prediksi dan data asli
+        fig, ax = plt.subplots(figsize=(12, 6))
+        ax.plot(predicted_data.index, predicted_data['Actual'], label='Actual')
+        ax.plot(predicted_data.index, predicted_data['Predicted'], label='Predicted')
+        ax.plot(data.index, data['harga_current'], label='Train Data', linestyle='dashed', alpha=0.5)
+        ax.set_title('Hasil Prediksi vs. Data Asli\nRMSE: {:.2f}'.format(rmse))
+        ax.set_xlabel('Tanggal')
+        ax.set_ylabel('Harga')
+        ax.legend()
 
-        # Reset index kolom
-        data_predictions.reset_index(drop=True, inplace=True)
-        data_test_df.reset_index(drop=True, inplace=True)
+        # Simpan gambar sebagai file PNG (tanpa perlu menyimpan dalam objek BytesIO)
+        plot_filename = 'plot.png'
+        plt.savefig(plot_filename, format='png')
+        plt.close()
 
-        # Penggabungan data asli dengan data prediksi
-        dataset = pd.concat([data_test_df, data_predictions], axis=1)
-
-        return dataset
+        # Mengirimkan gambar ke browser
+        return send_file(plot_filename, mimetype='image/png')
     except pymysql.MySQLError as err:
         abort(HTTPStatus.INTERNAL_SERVER_ERROR, description=str(err))
     except Exception as e:
         abort(HTTPStatus.BAD_REQUEST, description=str(e))
-
-# Add new predict data
-@app.route(f"{route_prefix}/addPredict", methods=['GET'])
-def addPredict():
-    try:
-        data_json = 123
-        return jsonify(data_json)
-    except pymysql.MySQLError as err:
-        abort(HTTPStatus.INTERNAL_SERVER_ERROR, description=str(err))
-    except Exception as e:
-        abort(HTTPStatus.BAD_REQUEST, description=str(e))
-
-# Testing for connecting to database (/api/v1/health)
-@app.route(f"{route_prefix}/health", methods=['GET'])
-def health():
-    try:
-        print("Hello")
-        return "Halo tersampaikan"
-    except pymysql.MySQLError as err:
-        abort(HTTPStatus.INTERNAL_SERVER_ERROR, description=str(err))
-    except Exception as e:
-        abort(HTTPStatus.BAD_REQUEST, description=str(e))
-
-# /
-@app.route('/', methods=['GET'])
-def home():
-    return redirect(url_for('health'))
 # =================================================[ Routes - End ]
 
 # ================================[ Error Handler Defined - Start ]
